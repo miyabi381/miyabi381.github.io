fa0c192a9e0e839506e7359a5e147b7d7920054a
miyabi381 <makkun1011@icloud.com>
2022-09-13 17:05:00 +0900

    ゴーストの正体機能デモ

--- js/main.js
+++ js/main.js
@@ -67,25 +67,113 @@ function PPGhostText(idName) {
  * 引数１：証拠ID
  * 引数２：ゴーストID
  */
-function PPGhostJudge(evi,eviCheck, ghost) {
-    let objEvi = document.getElementById(evi);
-    let objEviCheck = document.getElementById(eviCheck);
-    let objGhost = document.getElementById(ghost);
-
-    if (objEviCheck.className == "checkbox") {  // yes
-        objGhost.style.color = "";
-        objGhost.style.border = "";
-
-    } else if(objEvi.className == "line-through"){  // no
-        objGhost.style.color = "#ccc9c4";
-        objGhost.style.borderColor = "#ccc9c4";
-
-    }else{
-        objGhost.style.color = "";  // none
-        objGhost.style.border = "";
+function PPGhostJudge(eviID, ghostID) {
+    let objEvi = document.getElementById(eviID);
+    if (objEvi.className == "line-through") {
+        for (const val of ghostID) {
+            const objGhost = document.getElementById(val);
+            objGhost.style.color = "#ccc9c4";  // no
+            objGhost.style.borderColor = "#ccc9c4";
+        }
+    } else {
+        for (const val of ghostID) {
+            const objGhost = document.getElementById(val);
+            objGhost.style.color = "";  // none
+            objGhost.style.border = "";
+        }
     }
-
 }
+
+/**
+ * PhasmoPhobia 証拠別ゴースト配列
+ */
+// EMFレベル５
+let emfEvi = [
+    "spirit", "wraith", "jinn", "shade", "oni", "goryo", "myling", "twins", "raiju", "obake"
+];
+
+// D.O.T.S プロジェクター
+let dotsEvi = [
+    "wraith", "phantom", "banshee", "yurei", "oni", "yokai", "goryo", "raiju", "deogen", "thaye"
+];
+
+// 指紋
+let shimonEvi = [
+    "phantom", "polter", "banshee", "jinn", "demon", "hantu", "goryo", "myling", "obake", "mimic"
+];
+
+// ゴーストオーブ
+let orbEvi = [
+    "banshee", "mare", "revenant", "yurei", "hontu", "yokai", "onryo", "raiju", "obake", "thaye"
+];
+
+// ゴーストライティング
+let bookEvi = [
+    "spirit", "polter", "mare", "revenant", "shade", "demon", "myling", "moroi", "deogen", "thaye"
+];
+
+// スピリットボックス
+let boxEvi = [
+    "spirit", "wraith", "phantom", "polter", "mare", "yokai", "onryo", "twins", "moroi", "deogen", "mimic"
+];
+
+// 氷点下の温度
+let iceEvi = [
+    "jinn", "revenant", "shade", "demon", "yurei", "oni", "hantu", "onryo", "twins", "moroi", "mimic"
+];
+
+/**
+ * PhasmoPhobia ゴースト別ゴースト配列
+ */
+// スピリット
+let spritEvi= ["emf","box","book"];
+// レイス
+let wraith= ["emf","box","dots"];
+// ファントム
+let phantom= ["box","shimon","dots"];
+// ポルターガイスト
+let polter= ["box","shimon","book"];
+// バンシー
+let banshee= ["shimon","orb","dots"];
+// ジン
+let jinn= ["emf","shimon","ice"];
+// メアー
+let mare= ["box","orb","book"];
+// レヴナント
+let revenant= ["orb","book","ice"];
+// シェード
+let shade= ["emf","book","ice"];
+// デーモン
+let demon= ["shimon","book","ice"];
+// 幽霊
+let yurei= ["orb","ice","dots"];
+// 鬼
+let oni= ["emf","ice","dots"];
+// 妖怪
+let yokai= ["box","orb","dots"];
+// ハントゥ
+let hantu= ["shimon","orb","ice"];
+// 御霊
+let goryo= ["emf","shimon","dots"];
+// マイリング
+let myling= ["emf","shimon","book"];
+// 怨霊
+let onryo= ["box","orb","ice"];
+// ツインズ
+let twins= ["emf","box","ice"];
+// 雷獣
+let raiju= ["emf","orb","dots"];
+// 化け狐
+let obake= ["emf","shimon","orb"];
+// ミミック
+let mimic= ["box","shimon","ice"];
+// モーロイ
+let moroi= ["box","book","ice"];
+// デオヘン
+let deogen= ["box","book","dots"];
+// セーイ
+let thaye= ["orb","book","dots"];
+
 
 
 /**
