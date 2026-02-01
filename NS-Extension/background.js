6db8bc7ed770dd9d8a466d8304dac18e0b1506b4
miyabi381 <makkun1011@icloud.com>
2026-02-01 17:22:14 +0900

    js読み込み直し設定追加

---
+++ NS-Extension/background.js
@@ -0,0 +1,18 @@
+const browser = self.browser || self.chrome;
+const TARGETS = [
+    "https://miyabi381.github.io/Scheduler-test.html",
+    "https://x.com/"
+];
+
+
+browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.status !== "complete") return;
+    if (!tab.url || !tab.url.startsWith("http")) return;
+
+    if (!TARGETS.some(url => tab.url.startsWith(url))) return;
+
+    browser.scripting.executeScript({
+        target: { tabId },
+        files: ["common.js", "main.js"]
+    });
+});
