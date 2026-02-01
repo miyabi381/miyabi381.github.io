4d286a24558fa19401ac6e7deac108138e627b53
miyabi381 <makkun1011@icloud.com>
2026-02-02 07:20:17 +0900

    クリックイベント関数設定

--- NS-Extension/common.js
+++ NS-Extension/common.js
@@ -1,4 +1,16 @@
 //　ログ出力
-function cLog(text){
-    console.log(text);
+function cLog(text) {
+	console.log(text);
+}
+
+// クリックイベント
+function humanClick(target) {
+	const events = ['mousedown', 'mouseup', 'click'];
+	events.forEach(type =>
+		target.dispatchEvent(new MouseEvent(type, {
+			bubbles: true,
+			cancelable: true,
+			view: window
+		}))
+	);
 }
--- NS-Extension/main.js
+++ NS-Extension/main.js
@@ -1,1 +1,5 @@
-cLog("tomato");
+cLog("NS-Extension : 読み込み完了");
+
+// 一覧表示ボタン
+const itiranbtn = document.querySelector("body > div > div:nth-child(4) > button");
+humanClick(itiranbtn);
